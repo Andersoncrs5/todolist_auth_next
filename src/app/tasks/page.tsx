340b6a595b0a1d3361160f6d583f22f075efa7a4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-30 14:00:48 -0300

    set filter, btns, improvement in style and another thing

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import BtnUrl from "@/components/btnUrl/btnUrl.component"
-import { IoMdCreate } from "react-icons/io"
+import { IoIosLogOut, IoMdCreate } from "react-icons/io"
 import { UseTasks } from "./tasks.hook"
 import TaskModel from "@/model/task/task.model"
 import ShowTask from "@/components/showTask/showTask.component"
 import ActionMenu from "@/components/ActiveMenu/activeMenu.component"
 import Btn from "@/components/btn/btn.component"
-import { MdDelete } from "react-icons/md"
+import { MdDelete, MdSystemUpdateAlt } from "react-icons/md"
 import { TfiReload } from "react-icons/tfi"
 import ShowAlert from "@/components/showAlert/showAlert.component"
+import CustomInput from "@/components/customInput/customInput.component"
 
 export default function Tasks() {
 
@@ -22,7 +23,11 @@ export default function Tasks() {
         msg,
         tasks,
         deleteTask,
-        changeStatus
+        changeStatus,
+        query,
+        setQuery,
+        logout,
+        updateTask
     } = UseTasks()
 
     return (
@@ -38,24 +43,73 @@ export default function Tasks() {
                         <div className={"basis-6/10"} >
 
                         </div>
-                        <div className={"basis-2/10 text-center"} >
-                            <BtnUrl 
-                                border={"border-white"} 
-                                hoverBg={"hover:bg-white"} 
-                                text={"text-white"} 
-                                bgColor={"bg-transparent"} 
-                                url={"/tasks/create"} 
-                                pdd={"p-3"} 
-                                hoverText={"hover:text-black"}      
-                                icon={ <IoMdCreate /> }
-                                w={"w-10"}
-                                margin="mt-1"
-                            />
+                        <div className={"basis-2/10 text-center flex flex-row justify-center  space-x-1"} >
+                            <div>
+                                <BtnUrl 
+                                    border={"border-white"} 
+                                    hoverBg={"hover:bg-white"} 
+                                    text={"text-white"} 
+                                    bgColor={"bg-transparent"} 
+                                    url={"/tasks/create"} 
+                                    pdd={"p-3"} 
+                                    hoverText={"hover:text-black"}      
+                                    icon={ <IoMdCreate /> }
+                                    margin="mt-1"
+                                />
+                            </div>
+                            <div>
+                                <Btn 
+                                    padding={"p-3"} 
+                                    border={"border-white"} 
+                                    fn={() => logout()} 
+                                    hover={"hover:bg-white"} 
+                                    text={"text-white"}  
+                                    icon={ <IoIosLogOut /> } 
+                                    margin="mt-1"                
+                                />
+                            </div>
+                            
+                            
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-row w-screen h-screen">
-                    <div className="border border-white basis-2/10 ">01</div>
+                    <div className="border border-white basis-2/10 flex flex-col">
+                        <form className="m-4 flex flex-col space-y-2" >
+                            <div>
+                                <CustomInput 
+                                    value={query.Title || ''} 
+                                    onChange={(e) => setQuery(prevQuery => ({ ...prevQuery, Title: e.target.value })) } 
+                                    type={"text"} 
+                                    padding={"p-1.5"}    
+                                    border={"border-white"}
+                                    nameLabel={"Title"}
+                                    textColor={"text-white"}
+                                />
+                            </div>
+                            <div >
+                                <label className="text-white">Status DOne</label>
+                                <select
+                                    className="border text-white rounded p-2 border border-white w-[100%]"
+                                    value={
+                                    query.Done === undefined ? "none" : query.Done ? "true" : "false"
+                                    }
+                                    onChange={(e) => {
+                                    const value = e.target.value;
+                                    setQuery((prev) => ({
+                                        ...prev,
+                                        Done: value === "none" ? undefined : value === "true",
+                                    }));
+                                    }}
+                                >
+                                    <option className="text-black bg-blue-500 " value="none">NONE</option>
+                                    <option className="text-black bg-blue-500 " value="true">TRUE</option>
+                                    <option className="text-black bg-blue-500 " value="false">FALSE</option>
+                                </select>
+                            </div>
+
+                        </form>
+                    </div>
                     <div className="border border-white basis-8/10 ">
                         <div className="flex items-center justify-center mt-3">
 
@@ -93,7 +147,17 @@ export default function Tasks() {
                                                                 fn={() => changeStatus(e.id)} 
                                                                 hover={"hover:bg-white"} 
                                                                 text={"text-white"}   
-                                                                icon={ <TfiReload /> }                                                
+                                                                icon={ <TfiReload /> } 
+                                                                disabled={false}                                               
+                                                            />
+                                                            <Btn 
+                                                                padding={"p-3"} 
+                                                                border={"border-white"} 
+                                                                fn={() => updateTask(e.id)} 
+                                                                hover={"hover:bg-white"} 
+                                                                text={"text-white"}   
+                                                                icon={ <MdSystemUpdateAlt /> } 
+                                                                disabled={false}                                               
                                                             />
                                                         </div>   
                                                     </ActionMenu>
