5f0f77b486ed506e5642913250f7f861957e95e4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-30 14:04:18 -0300

    New type to hover text

---
+++ src/types/hoverText.type.ts
@@ -0,0 +1,8 @@
+export type HoverTextStyleType = 
+  | 'hover:text-red-500'
+  | 'hover:text-white'
+  | 'hover:text-yellow-500'
+  | 'hover:text-blue-500'
+  | 'hover:text-green-500'
+  | 'hover:text-purple-500'
+  | 'hover:text-black'; 
