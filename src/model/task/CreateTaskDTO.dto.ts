0e5db274023a9ae8ab243c192c362db55722f830
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-30 14:03:16 -0300

    new dto to Create task and another to update

---
+++ src/model/task/CreateTaskDTO.dto.ts
@@ -0,0 +1,5 @@
+export default interface CreateTaskDTO {
+    title: string
+    description: string
+    done: boolean
+}
---
+++ src/model/task/UpdateTaskDTO.dto.ts
@@ -0,0 +1,5 @@
+export default interface UpdateTaskDTO {
+    title: string
+    description: string
+    done: boolean
+}
