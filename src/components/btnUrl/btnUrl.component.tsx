1c3e11487d35ba89d3a3524bb12ca1128ecdb729
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-29 21:44:06 -0300

    change struct btn url component

--- src/components/btnUrl/btnUrl.component.tsx
+++ src/components/btnUrl/btnUrl.component.tsx
@@ -1,44 +1,55 @@
+"use client"
+
+import { useRouter } from "next/navigation"
 import { BgStyleType } from "@/types/bg.type"
 import { BorderStyleType } from "@/types/border.types"
 import { HoverBgStyleType } from "@/types/hoverBg.type"
 import { HoverTextStyleType } from "@/types/hoverText.type"
 import { TextStyleType } from "@/types/text.type"
-import Link from "next/link"
+import { ReactNode } from "react"
 
 interface Props {
-    border: BorderStyleType
-    margin?: string
-    hoverBg: HoverBgStyleType
-    text: TextStyleType
-    bgColor: BgStyleType
-    url: string
-    name?: string
-    pdd: PaddingStyleType
-    hoverText: HoverTextStyleType
-    
+  border: BorderStyleType
+  margin?: string
+  hoverBg: HoverBgStyleType
+  text: TextStyleType
+  bgColor: BgStyleType
+  url: string
+  name?: string
+  pdd: PaddingStyleType
+  hoverText: HoverTextStyleType
+  icon?: ReactNode
+  w: string
 }
 
-export default function BtnUrl({ 
-    border, 
-    text, 
-    hoverBg,
-    bgColor, 
-    url, 
-    name, 
-    pdd, 
-    margin, 
-    hoverText 
+export default function BtnUrl({
+  border,
+  text,
+  hoverBg,
+  bgColor,
+  url,
+  name,
+  pdd,
+  margin,
+  hoverText,
+  icon,
+  w,
 }: Props) {
-    return (
-        <Link 
-        href={url}
-        className={`
-            ${bgColor} border ${border}
-            ${pdd} rounded ${text} ${hoverBg}
-            ${hoverText} ${margin} 
-        `}
-        >
-            {name}
-        </Link>
-    )
+  const router = useRouter()
+
+  return (
+    <button
+      type="button"
+      onClick={() => router.push(url)}
+      className={`
+        flex items-center gap-2
+        ${bgColor} ${border ? `border ${border}` : ""}
+        ${pdd} rounded ${text} ${hoverBg}
+        ${hoverText} ${margin || ""}
+      `}
+    >
+      {icon && <span>{icon}</span>}
+      {name}
+    </button>
+  )
 }
