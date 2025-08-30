ec823c63bb9d678fa837e9b4479d081dd1b2a1f0
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-29 21:43:00 -0300

    hook to activeMenu

---
+++ src/components/ActiveMenu/useActionMenu.component.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect, useRef, useState } from "react";
+
+export function useActionMenu() {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return {
+        isOpen,
+        setIsOpen,
+        menuRef
+    };
+}
