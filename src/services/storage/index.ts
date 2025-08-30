147bed7349a244aa14ad8fd6f08f6e5616776c1a
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-29 21:45:36 -0300

    Service to handle localstorage

---
+++ src/services/storage/index.ts
@@ -0,0 +1,29 @@
+import Tokens from "@/util/res/Token.res";
+
+export default class LocalStorageService {
+    private _token: string = "token"
+    private _refreshToken: string = "refreshToken"
+
+    setTokens(tokens: Tokens) {
+        localStorage.setItem(this._token, tokens.token)
+        localStorage.setItem(this._refreshToken, tokens.refreshToken)
+
+        if (tokens.expirationRefreshToken) {
+            localStorage.setItem("expirationRefreshToken", tokens.expirationRefreshToken.toISOString())
+        }
+
+        if (tokens.expirationToken) {
+            localStorage.setItem("expirationToken", tokens.expirationToken.toISOString())
+        }
+
+    }
+
+    getToken(): string | null {
+        return localStorage.getItem(this._token)
+    }
+
+    getRefreshToken(): string | null {
+        return localStorage.getItem(this._refreshToken)
+    }
+
+}
