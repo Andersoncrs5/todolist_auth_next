20a208b4fb9d390658e2354df322001b4b350ab3
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-29 21:46:04 -0300

    new type to border

---
+++ src/types/border.types.ts
@@ -0,0 +1,8 @@
+export type BorderStyleType =
+  | 'border-red-500'
+  | 'border-white'
+  | 'border-yellow-500'
+  | 'border-blue-500'
+  | 'border-green-500'
+  | 'border-purple-500'
+  | 'border-black'; 
