914a1e76c0b8537752657658f6c8f5ff099daa1f
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-08-30 14:04:05 -0300

    New type to hover bg-color

---
+++ src/types/hoverBg.type.ts
@@ -0,0 +1,8 @@
+export type HoverBgStyleType = 
+  | 'hover:bg-red-500'
+  | 'hover:bg-white'
+  | 'hover:bg-yellow-500'
+  | 'hover:bg-blue-500'
+  | 'hover:bg-green-500'
+  | 'hover:bg-purple-500'
+  | 'hover:bg-transparent';
